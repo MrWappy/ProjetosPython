63ec7d6a639c519d6ca3ba2897fc594a3bbe389a
Wappy <92633701+MrWappy@users.noreply.github.com>
2021-11-27 20:03:20 +0000

    Delete TotalLitros.py

--- TotalLitros.py
+++
@@ -1,8 +0,0 @@
-print("Introduza o tempo:")
-t = int(input())
-print("Introduza a velocidade:")
-v = int(input())
-dist = t * v
-l = dist + 8/100
-print(f"A distância é {dist}Km e o total de litros é {l}L")
-
